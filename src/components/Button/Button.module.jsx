0e3c14f29c52831dfbfadf63874c8e28d4a408e0
VladislavSemenets <124085061+VladislavSemenets@users.noreply.github.com>
2023-09-17 17:09:58 +0300

    finished

--- src/components/Button/Button.module.jsx
+++ src/components/Button/Button.module.jsx
@@ -6,36 +6,14 @@ export const ButtonLoad = styled.button`
 
   line-height: 100%;
   text-align: center;
-  color: #ffffff;
+  color: white;
 
   border-radius: 12px;
 
-  background: linear-gradient(180deg, #40df9f 0%, #3ed598 100%);
-  box-shadow: 0px 2px 4px rgba(15, 218, 137, 0.3);
-
-  transition: background 250ms cubic-bezier(0.4, 0, 0.2, 1);
-
+  background: #005eff;
+  
   :hover,
   :focus {
-    background: #286053;
+    background: #01296e;
   }
-
-  // padding: 8px 16px;
-  // border-radius: 2px;
-  // background-color: #3f51b5;
-  // transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  // text-align: center;
-  // display: inline-block;
-  // color: #fff;
-  // border: 0;
-  // text-decoration: none;
-  // cursor: pointer;
-  // font-family: inherit;
-  // font-size: 18px;
-  // line-height: 24px;
-  // font-style: normal;
-  // font-weight: 500;
-  // min-width: 180px;
-  // box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-  //   0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
 `;
